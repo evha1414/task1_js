f8683fba0adae718c1ee2ab64701537e11561d87
Vanesa <vanesasolterman@gmail.com>
2023-02-26 23:31:31 -0300

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,15 @@
-let object ={
-    name: "",
+const contenedorTarjetas = document.querySelector('#contenedor')
+
+let tarjetasGeneradas = crearTarjetas(personas)
+
+contenedorTarjetas.innerHTML = tarjetasGeneradas
+
+function crearTarjetas(arrayDatos){
+    let tarjetas = ''
+    for (const persona of arrayDatos) {
+        tarjetas += <div class="card text-black" style=
+        <h5 class="card-ttle">${persona.name}</h5>
+        <p class="card-text text-danger flex-grow-1"${persona.}
+    }
+    return tarjetas
 }
-
-console.log(object);
-
-object.name = ""
-
-let date = "date"
-
-console.log(date)
-
-let description = "description"
-
-console.log(description)
-
-let category = "category"
-
-console.log(category)
-
-let place = "place"
-
-console.log(place)
-
-let capacity = "capacity"
-
-console.log(capacity)
-
-let asssitanceOrEstimate = "assistanceOrEstimate"
-
-console.log(asssitanceOrEstimate)
-
-let price = "price"
-
-console.log(price)
-

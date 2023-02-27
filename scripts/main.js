2d17f41582ba954fd90513404ff80f13bddfa33f
Vanesa <vanesasolterman@gmail.com>
2023-02-26 22:22:19 -0300

    js

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+let object ={
+    name: "",
+}
+
+console.log(object);
+
+object.name = ""
+
+let date = "date"
+
+console.log(date)
+
+let description = "description"
+
+console.log(description)
+
+let category = "category"
+
+console.log(category)
+
+let place = "place"
+
+console.log(place)
+
+let capacity = "capacity"
+
+console.log(capacity)
+
+let asssitanceOrEstimate = "assistanceOrEstimate"
+
+console.log(asssitanceOrEstimate)
+
+let price = "price"
+
+console.log(price)
+

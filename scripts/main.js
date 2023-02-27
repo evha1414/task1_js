303155640000ef54aab8c5093ba327c6308ba8a6
Vanesa <vanesasolterman@gmail.com>
2023-02-27 13:17:01 -0300

    data

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,26 @@
 const contenedorTarjetas = document.querySelector('#contenedor')
 
-let tarjetasGeneradas = crearTarjetas(personas)
+let tarjetasGeneradas = crearTarjetas(data)
 
 contenedorTarjetas.innerHTML = tarjetasGeneradas
 
 function crearTarjetas(arrayDatos){
     let tarjetas = ''
-    for (const persona of arrayDatos) {
-        tarjetas += <div class="card text-black" style=
-        <h5 class="card-ttle">${persona.name}</h5>
-        <p class="card-text text-danger flex-grow-1"${persona.}
+    for (const data of arrayDatos) {
+        tarjetas += '<div class="card" style="width: 18rem; height: 21rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="fw-bold">${data.name}</h5>
+          <p class="fst-italic">${data.date}</p>
+          <p class="fw-semibold">${data.description}</p>
+          <p class="card-text">${data.category}</p>
+          <p class="text-decoration-underline">${data.place}</p>
+          <p class="fw-light">${data.capacity}</p>
+          <p class="fw-light">${data.asssitance}</p>
+          <p class="position-absolute bottom-1 start-1">${data.price}</p>
+          <a href="#" class="btn btn-secondary position-absolute bottom-1 end-1"></a>
+        </div>
+      </div>'
     }
     return tarjetas
 }

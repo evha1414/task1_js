e3c059078008eb2e1e85813eb795d69f73646757
Vanesa <vanesasolterman@gmail.com>
2023-02-27 21:03:17 -0300

    filter

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,24 @@
 const contenedorTarjetas = document.querySelector('#contenedor')
 
-let tarjetasGeneradas = crearTarjetas(data)
+let tarjetasGeneradas = crearTarjetas(data.events)
 
 contenedorTarjetas.innerHTML = tarjetasGeneradas
 
 function crearTarjetas(arrayDatos){
     let tarjetas = ''
-    for (const data of arrayDatos) {
+    for (const events of arrayDatos) {
         tarjetas += `<div class="card" style="width: 18rem; height: 21rem;">
         <img src="..." class="card-img-top rounded-4 p-2" alt="...">
         <div class="card-body">
-          <h5 class="fw-bold">${data.name}</h5>
-          <p class="fst-italic">${data.date}</p>
-          <p class="fw-semibold">${data.description}</p>
-          <p class="card-text">${data.category}</p>
-          <p class="text-decoration-underline">${data.place}</p>
-          <p class="fw-light">${data.capacity}</p>
-          <p class="fw-light">${data.asssitance}</p>
-          <p class="position-absolute bottom-1 start-1">${data.price}</p>
-          <a href="#" class="btn btn-secondary position-absolute bottom-1 end-1"></a>
+          <h5 class="fw-bold">${events.name}</h5>
+          <p class="fst-italic">${events.date}</p>
+          <p class="fw-semibold">${events.description}</p>
+          <p class="card-text">${events.category}</p>
+          <p class="text-decoration-underline">${events.place}</p>
+          <p class="fw-light">${events.capacity}</p>
+          <p class="fw-light">${events.assitance}</p>
+          <p class="position-absolute bottom-1 start-1">${events.price}</p>
+          <a href="#" class="btn btn-secondary position-absolute bottom-1 end-1">${events.button}</a>
         </div>
       </div>`
     }
